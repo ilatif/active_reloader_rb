ee790a4ffa3467570bbf7ea26afa8d4dd0dd1f1b
ilatif <ilatif.bwp@gmail.com>
2015-01-07 23:50:13 +0500

    Running active_reloader server on rails_server_port + 1

--- lib/active_reloader.rb
+++ lib/active_reloader.rb
@@ -9,7 +9,8 @@ end
 if (defined?(Rails::Server))
 	Thread.new do 
 		server_script_path = File.expand_path("../../config.ru", __FILE__)
+		port = Rails::Server.new.options[:Port] + 1
 		puts "\n\n\n\n\n\n =========#{server_script_path}\n\n\n\n"
-		`rackup '#{server_script_path}'`
+		`rackup '#{server_script_path}' -p #{port}`
 	end
 end
