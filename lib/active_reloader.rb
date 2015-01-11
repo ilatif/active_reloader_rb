c4c14cf151c100a4105c3548d6784ace66bec4f8
ilatif <ilatif.bwp@gmail.com>
2015-01-12 00:27:05 +0500

    Not adding code of active_reloader.js in production environment or in rake assets:* tasks

--- lib/active_reloader.rb
+++ lib/active_reloader.rb
@@ -7,7 +7,7 @@ module ActiveReloader
 end
 
 # Code to run active_reloader server
-if (defined?(Rails::Server))
+if (defined?(Rails::Server) && Rails.env.development?)
 	Thread.new do 
 		server_script_path = File.expand_path("../../config.ru", __FILE__)
 		port = Rails::Server.new.options[:Port] + 1
@@ -15,4 +15,7 @@ if (defined?(Rails::Server))
 		puts "\n\n\n\n\n\n =========#{server_script_path}\n\n\n\n"
 		`rackup '#{server_script_path}' -p #{port}`
 	end
+	File.write(File.expand_path("../../app/assets/javascripts/active_reloader.js", __FILE__), "//= require active_reloader_initializer");
+else
+	File.write(File.expand_path("../../app/assets/javascripts/active_reloader.js", __FILE__), "");
 end
